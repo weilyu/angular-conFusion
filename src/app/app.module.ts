a5d26c8ec987248bca496238de141bc3c6116a43
Wei Lyu <lvwei1001@live.com>
2018-03-10 18:07:24 +0800

    basic services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,8 +11,8 @@ import 'hammerjs';
 
 import {AppComponent} from './app.component';
 import {MenuComponent} from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
-
+import {DishdetailComponent} from './dishdetail/dishdetail.component';
+import {DishService} from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +28,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
