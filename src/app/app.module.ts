ae3917db3e5172f7b12c927a1ce297a4881ac173
Wei Lyu <lvwei1001@live.com>
2018-03-12 22:48:16 +0800

    angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,9 +12,15 @@ import 'hammerjs';
 import {AppComponent} from './app.component';
 import {MenuComponent} from './menu/menu.component';
 import {DishdetailComponent} from './dishdetail/dishdetail.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {AboutComponent} from './about/about.component';
+import {HomeComponent} from './home/home.component';
+import {ContactComponent} from './contact/contact.component';
+
 import {DishService} from './services/dish.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+
+import {AppRoutingModule} from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -22,7 +28,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +39,8 @@ import { FooterComponent } from './footer/footer.component';
     FormsModule,
     HttpModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    AppRoutingModule
   ],
   providers: [DishService],
   bootstrap: [AppComponent]
