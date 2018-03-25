48d9204e5693b479ccef3169b8bdec4c27e5aa60
Wei Lyu <lvwei1001@live.com>
2018-03-25 16:24:43 +0800

    promise part 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,7 +15,7 @@ export class AboutComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.leaders = this.leaderService.getLeaders();
+    this.leaderService.getLeaders().then(leaders => this.leaders = leaders);
   }
 
 }
