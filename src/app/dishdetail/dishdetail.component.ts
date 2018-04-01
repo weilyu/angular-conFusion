fdf10713ed4978558c9ef23ad2c1238e8574c53c
Wei Lyu <lvwei1001@live.com>
2018-04-01 20:18:49 +0800

    add errmsg to dishdetail

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -20,6 +20,8 @@ export class DishdetailComponent implements OnInit {
   prev: number;
   next: number;
 
+  errMess: string;
+
   commentForm: FormGroup;
 
   formErrors = {
@@ -52,7 +54,7 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dish => {
         this.dish = dish;
         this.setPrevNext(dish.id);
-      });
+      }, errmess => this.errMess = <any>errmess);
   }
 
   setPrevNext(dishId: number) {
