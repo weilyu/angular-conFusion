90f86ea0d7c5447eb475db7973ab0e3f549ce327
Wei Lyu <lvwei1001@live.com>
2018-04-01 18:48:01 +0800

    done promotion service

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -6,23 +6,33 @@ import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/observable/of';
+import {Http} from '@angular/http';
+import {ProcessHTTPMsgService} from './process-httpmsg.service';
+import {baseURL} from '../shared/baseurl';
 
 @Injectable()
 export class PromotionService {
 
-  constructor() {
+  constructor(private http: Http,
+              private processHTTPMsgService: ProcessHTTPMsgService) {
   }
 
   getPromotions(): Observable<Promotion[]> {
-    return Observable.of(PROMOTIONS).delay(2000);
+    return this.http.get(baseURL + 'promotions').map(res => {
+      return this.processHTTPMsgService.extractData(res);
+    });
   }
 
   getPromotion(id: number): Observable<Promotion> {
-    return Observable.of(PROMOTIONS.filter((promotion) => (promotion.id === id))[0]).delay(2000);
+    return this.http.get(baseURL + 'promotions/' + id).map(res => {
+      return this.processHTTPMsgService.extractData(res);
+    });
   }
 
   getFeaturedPromotion(): Observable<Promotion> {
-    return Observable.of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).delay(2000);
+    return this.http.get(baseURL + 'promotions?featured=true').map(res => {
+      return this.processHTTPMsgService.extractData(res)[0];
+    });
   }
 
 }
