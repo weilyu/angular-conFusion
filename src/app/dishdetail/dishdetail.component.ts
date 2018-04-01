26e57609a81021d2f2d03b0259dad6d37084263f
Wei Lyu <lvwei1001@live.com>
2018-04-01 22:38:44 +0800

    organize animation

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/switchMap';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Comment} from '../shared/comment';
 import {trigger, state, style, animate, transition} from '@angular/animations';
+import {visibility} from '../animations/app.animation';
 
 
 @Component({
@@ -14,17 +15,7 @@ import {trigger, state, style, animate, transition} from '@angular/animations';
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
   animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility()
   ]
 })
 export class DishdetailComponent implements OnInit {
